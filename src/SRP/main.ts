0b7e7284e53ce3664abc5431dbbf44515d755fec
KevenSoares22 <keven.v.soares@gmail.com>
2023-06-09 14:40:31 -0300

    More SRP updates

--- src/SRP/main.ts
+++ src/SRP/main.ts
@@ -1,17 +1,20 @@
+import { Messaging } from "./messaging";
 import { Order } from "./order";
+import { Products } from "./products";
+import { SaveOrder } from "./saveOrder";
 import { ShoppingCart } from "./shoppingCart";
 
 
 const shoppingCart = new ShoppingCart();
-const order = new Order(shoppingCart);
+const messaging = new Messaging();
+const saveOrder = new SaveOrder();
+const order = new Order(shoppingCart, messaging, saveOrder);
 
-
-
-shoppingCart.addProduct({ name: 'Pen', price: 1.2 })
-shoppingCart.addProduct({ name: 'Book', price: 5.8 })
-shoppingCart.addProduct({ name: 'Mouse', price: 10.6 })
-shoppingCart.addProduct({ name: 'Monitor', price: 980.2 })
-shoppingCart.addProduct({ name: 'Keyboard', price: 120.8 })
+shoppingCart.addProduct(new Products('Pen', 1.2))
+shoppingCart.addProduct(new Products('Book', 5.8))
+shoppingCart.addProduct(new Products('Mouse', 10.6))
+shoppingCart.addProduct(new Products('Monitor', 980.2))
+shoppingCart.addProduct(new Products('Keyboard', 120.8))
 
 
 
--- src/SRP/order.ts
+++ src/SRP/order.ts
@@ -1,41 +1,36 @@
 
 import { OrderStatus } from './interfaces/orderStatus';
+import { Messaging } from './messaging';
+import { SaveOrder } from './saveOrder';
 import { ShoppingCart } from './shoppingCart';
 
 
 export class Order {
-private _orderStatus: OrderStatus = 'open';
-constructor(private readonly cart: ShoppingCart){}
+    private _orderStatus: OrderStatus = 'open';
+    constructor(
+            private readonly cart: ShoppingCart,
+            private readonly messaging: Messaging,
+            private readonly saveOrder: SaveOrder
+        ){}
 
 
-    
-get orderStatus(): OrderStatus{
-    return this._orderStatus;
+    get orderStatus(): OrderStatus{
+        return this._orderStatus;
 
-}
-
-
-sendMessage(msg: string): void {
-    console.log(`Message Sent: ${msg}`);   
-}
-
-saveOrder(): void {
-    console.log('Your order has been saved!');   
-}   
-
-checkout(): void {
-    if (this.cart.isEmpty()) {
-        console.log('Your cart is empty');
-        return;   
     }
 
-    this._orderStatus = 'closed';
-    this.sendMessage('Your order has been recevied');
-    this.saveOrder();
-    this.cart.clearCart();
+    checkout(): void {
+        if (this.cart.isEmpty()) {
+            console.log('Your cart is empty');
+            return;   
+        }
 
+        this._orderStatus = 'closed';
+        this.messaging.sendMessage('Your order has been recevied');
+        this.saveOrder.saveOrder();
+        this.cart.clearCart();
 
-}
+    }
 
 }
 
